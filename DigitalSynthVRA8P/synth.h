fcc9fcf362df1606a486dc9156d4dc57e929a2d1
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2016-03-02 21:35:15 +0900

    Update PRESETs

--- DigitalSynthVRA8P/synth.h
+++ DigitalSynthVRA8P/synth.h
@@ -32,10 +32,10 @@ public:
     ISynthCore<0>::control_change(OSC_SAW_SQ   , 0  );
     ISynthCore<0>::control_change(OSC_DETUNE   , 64 );
     ISynthCore<0>::control_change(AMP_GATE_ENV , 0  );
-    ISynthCore<0>::control_change(LPF_CUTOFF   , 0  );
-    ISynthCore<0>::control_change(LPF_RESONANCE, 127);
-    ISynthCore<0>::control_change(LPF_ENV_AMT  , 127);
-    ISynthCore<0>::control_change(ENV_D_S_A    , 127);
+    ISynthCore<0>::control_change(LPF_CUTOFF   , 64 );
+    ISynthCore<0>::control_change(LPF_RESONANCE, 64 );
+    ISynthCore<0>::control_change(LPF_ENV_AMT  , 64 );
+    ISynthCore<0>::control_change(ENV_D_S_A    , 64 );
   }
 
   INLINE static void receive_midi_byte(uint8_t b) {
