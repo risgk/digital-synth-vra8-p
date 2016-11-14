87926d0fce9217ed8743d6e580ead17e7b1938ea
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2016-11-14 22:00:50 +0900

    Add DEBUG Output

--- DigitalSynthVRA8Px/audio-out.h
+++ DigitalSynthVRA8Px/audio-out.h
@@ -4,10 +4,14 @@
 
 #include "common.h"
 
+//#define DEBUG
+
 template <uint8_t T>
 class AudioOut {
   static const int SPEAKER_PIN = 6;   // PD6 (OC0A)
   static const int LED_PIN     = 13;  // PB5
+
+  static uint8_t m_count;
 
 public:
   INLINE static void open() {
@@ -22,9 +26,22 @@ public:
     // Timer/Counter1 (10-bit Fast PWM, 15625 Hz)
     TCCR1A = 0x03;
     TCCR1B = 0x09;
+
+    // For DEBUG
+    m_count = 0;
+    UCSR0B |= _BV(TXEN0);
   }
 
   INLINE static void write(int8_t level) {
+#if defined(DEBUG)
+    // Output Channel Pressure
+    m_count++;
+    if (m_count == 0x80) {
+      UDR0 = 0xDF;
+    } else if (m_count == 0) {
+      UDR0 = TCNT1 >> 3;
+    }
+#endif
     if (TIFR1 & _BV(TOV1)) {
       // CPU BUSY
       PORTB |= _BV(5);
@@ -36,3 +53,5 @@ public:
     OCR0A = 0x80 - level;
   }
 };
+
+template <uint8_t T> uint8_t AudioOut<T>::m_count;
